61c9fd3f97b9c68cf6261329c221a66107c5432e
Артем Антонянц <aa.artem.0303@mail.ru>
2025-09-09 16:33:16 +0700

    feat: изначальный контент

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "./styles";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <h1>HELLO WORLD</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/styles.ts
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  @font-face {
+    font-family: 'Montserrat';
+    src: url('https://fonts.gstatic.com/s/montserrat/v25/JTUSjIg1_i6t8kCHKm459Wlhyw.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+  }
+`;
